b04b5562d808b546b9a7ccdcfaaddf61bce4f1ca
William Huebner <wilhueb@gmail.com>
2021-09-01 15:17:44 -0400

    add sonarr tags

--- add_tag.py
+++ add_tag.py
@@ -3,6 +3,7 @@ import argparse
 import yaml
 
 from radarr import Radarr
+from sonarr import Sonarr
 
 
 with open('config.yaml', 'rb') as f:
@@ -17,5 +18,7 @@ parser.add_argument('discord_id')
 args = parser.parse_args()
 
 radarr = Radarr(config['radarr']['url'], config['radarr']['api_key'])
+sonarr = Sonarr(config['sonarr']['url'], config['sonarr']['api_key'])
 
 radarr._radarr.create_tag(f'{args.nickname}: {args.discord_id}')
+sonarr._sonarr.create_tag(f'{args.nickname}: {args.discord_id}')
