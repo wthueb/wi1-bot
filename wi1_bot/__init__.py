15046632cbaba15a169b347c175b191ca8d8efdd
William Huebner <wilhueb@gmail.com>
2022-05-24 04:41:39 -0400

    Use pyproject.toml instead of setup.cfg

---
+++ wi1_bot/__init__.py
@@ -0,0 +1,11 @@
+try:
+    from ._version import version
+except ImportError:
+    try:
+        from setuptools_scm import get_version
+
+        version = get_version()
+    except (ImportError, LookupError):
+        version = "???"
+
+__version__ = version
