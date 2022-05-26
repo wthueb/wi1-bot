1f12cbdb820478c8c9232a469f34bd82093c4fb3
William Huebner <wilhueb@gmail.com>
2022-05-26 00:25:09 -0400

    Versioning stuff

--- wi1_bot/__init__.py
+++ wi1_bot/__init__.py
@@ -1,11 +1,8 @@
 try:
-    from ._version import version
-except ImportError:
-    try:
-        from setuptools_scm import get_version
+    from setuptools_scm import get_version
 
-        version = get_version()
-    except (ImportError, LookupError):
-        version = "???"
+    version = get_version()
+except (ImportError, LookupError):
+    version = "???"
 
 __version__ = version
