3a4b2e6e812694bcb86aaac96b0da5d92372419d
William Huebner <wilhueb@gmail.com>
2026-08-12 14:00:20 -0400

    chore: update flake.lock

--- flake.nix
+++ flake.nix
@@ -35,9 +35,12 @@
           treefmt = {
             projectRootFile = "flake.nix";
 
-            programs.nixfmt.enable = true;
-            programs.ruff-check.enable = true;
-            programs.ruff-format.enable = true;
+            programs = {
+              actionlint.enable = true;
+              nixfmt.enable = true;
+              ruff-check.enable = true;
+              ruff-format.enable = true;
+            };
           };
         };
     };
